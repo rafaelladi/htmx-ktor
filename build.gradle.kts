fa17d8f8cb80ecb0994dc5cf88be81b760e2921c
rafaeldietrich-fl <rafael.dietrich@fountainledge.com>
2023-10-15 20:09:24 -0300

    add github workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+
+    id("maven-publish")
 }
 
-group = "com.inshur"
-version = "1.0-SNAPSHOT"
+group = "com.dietrich"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -15,6 +17,24 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:2.3.5")
 
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "htmx-ktor"
+            url = uri("https://maven.pkg.github.com/rafaelladi/htmx-ktor")
+            credentials {
+                username = project.findProperty("gpr.user")?.toString() ?: System.getenv("GITHUB_USER")
+                password = project.findProperty("gpr.key")?.toString() ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
